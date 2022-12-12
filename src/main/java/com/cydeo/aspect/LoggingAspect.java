faef5c971b1c57b36b0728df3ba30935098970e2
Nmerkel <noramerkel0@gmail.com>
2022-12-12 16:12:40 -0500

    created @Pointcut execution

--- src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,9 +1,19 @@
 package com.cydeo.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
+  //  Logger logger= LoggerFactory.getLogger(LoggingAspect.class);
+    //  when adding @Slf4j annotation, it will create this line without having to write it
+
+    @Pointcut("execution(* com.cydeo.controller.ProjectController.*(..)) || execution(* com.cydeo.controller.TaskController.*(..))")
+    public void anyProjectAndTaskControllerPC(){}
 }
